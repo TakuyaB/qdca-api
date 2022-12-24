def35d4838b35614fbc0e3c67ae7b34d55b17dd6
Takuya.B <takuya.b.works@gmail.com>
2022-12-25 05:08:36 +0900

    #3 use SoftDeletes

--- html/database/migrations/2022_12_24_183514_create_quotations_table.php
+++ html/database/migrations/2022_12_24_183514_create_quotations_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('quotations', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title', 100);
             $table->integer('price');
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
     }
